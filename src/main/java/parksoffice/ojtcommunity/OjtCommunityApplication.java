9a9c6526df6b6bed14543522bbac3a8255c59683
CRISPYTYPER <jack3409@naver.com>
2025-02-16 18:01:55 +0900

    first commit

---
+++ src/main/java/parksoffice/ojtcommunity/OjtCommunityApplication.java
@@ -0,0 +1,13 @@
+package parksoffice.ojtcommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OjtCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OjtCommunityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/parksoffice/ojtcommunity/OjtCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package parksoffice.ojtcommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OjtCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
