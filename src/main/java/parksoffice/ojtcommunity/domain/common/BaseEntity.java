800f915bdafea4a24e6f8addab3a5883ae7ea6ed
CRISPYTYPER <jack3409@naver.com>
2025-02-17 14:09:06 +0900

    docs: update comment format of BaseEntity

--- src/main/java/parksoffice/ojtcommunity/domain/common/BaseEntity.java
+++ src/main/java/parksoffice/ojtcommunity/domain/common/BaseEntity.java
@@ -20,7 +20,7 @@ public abstract class BaseEntity { // 공통 필드를 가진 부모 클래스. 
 
     /**
      * 기본 키(PK)
-     * <p>자동 증가 전략(default: AUTO)을 사용하여 ID를 생성함.</p>
+     * <p>자동 증가 전략(default: AUTO)을 사용하여 ID를 생성한다.</p>
      */
     @Id
     @GeneratedValue //아직 DB를 설정하지 않아서 default인 AUTO로 둠
@@ -28,19 +28,19 @@ public abstract class BaseEntity { // 공통 필드를 가진 부모 클래스. 
 
     /**
      * 생성일 (INSERT 시 자동 설정)
-     * <p>엔티티가 처음 저장될 때 현재 시간을 자동으로 설정함.</p>
+     * <p>엔티티가 처음 저장될 때 현재 시간을 자동으로 설정한다.</p>
      */
     private LocalDateTime createdAt; // 생성일
 
     /**
      * 수정일 (UPDATE 시 자동 설정)
-     * <p>엔티티가 수정될 때 현재 시간을 자동으로 업데이트함.</p>
+     * <p>엔티티가 수정될 때 현재 시간을 자동으로 업데이트한다.</p>
      */
     private LocalDateTime updatedAt; // 수정일
 
     /**
      * 엔티티가 처음 저장되기 전에 실행되는 메서드
-     * <p>createdAt 값을 현재 시간으로 자동 설정함.</p>
+     * <p>createdAt 값을 현재 시간으로 자동으로 설정한다.</p>
      */
     @PrePersist // 엔티티 INSERT 전에 실행됨
     protected void onCreate() {
@@ -49,7 +49,7 @@ public abstract class BaseEntity { // 공통 필드를 가진 부모 클래스. 
 
     /**
      * 엔티티가 수정되기 전에 실행되는 메서드
-     * <p>updatedAt 값을 현재 시간으로 자동 설정함.</p>
+     * <p>updatedAt 값을 현재 시간으로 자동으로 설정한다.</p>
      */
     @PreUpdate // 엔티티 UPDATE 전에 실행됨
     protected void onUpdate() {
