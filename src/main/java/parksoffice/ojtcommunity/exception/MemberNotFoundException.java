369d8f7991f545851fb40219ee4e4e189bd0ced0
CRISPYTYPER <jack3409@naver.com>
2025-02-18 17:27:12 +0900

    docs: change comment format of MemberNotFoundException

--- src/main/java/parksoffice/ojtcommunity/exception/MemberNotFoundException.java
+++ src/main/java/parksoffice/ojtcommunity/exception/MemberNotFoundException.java
@@ -1,7 +1,7 @@
 package parksoffice.ojtcommunity.exception;
 
 /**
- * 회원을 찾지 못했을 때 발생하는 예외입니다.
+ * 회원을 찾지 못했을 때 발생하는 예외이다.
  */
 public class MemberNotFoundException extends RuntimeException {
   public MemberNotFoundException(String message) {
