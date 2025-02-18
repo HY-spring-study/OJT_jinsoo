8cbdab2d5b118d36b8697ff4d42358e55ca4b2a9
CRISPYTYPER <jack3409@naver.com>
2025-02-18 17:28:23 +0900

    feat: implement UpdateMemberDto

---
+++ src/main/java/parksoffice/ojtcommunity/dto/UpdateMemberDto.java
@@ -0,0 +1,18 @@
+package parksoffice.ojtcommunity.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UpdateMemberDto {
+
+    @NotBlank(message = "사용자 이름은 필수입니다.")
+    private String username;
+
+    @NotBlank(message = "비밀번호는 필수입니다.")
+    @Size(min = 4, message = "비밀번호는 최소 4자리 이상이어야 합니다.")
+    private String password;
+}
