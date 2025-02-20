c72526e24bd6b5906d02bb0f161493b66f2b80c1
CRISPYTYPER <jack3409@naver.com>
2025-02-20 11:34:10 +0900

    feat: implement PostNotFoundException

---
+++ src/main/java/parksoffice/ojtcommunity/exception/PostNotFoundException.java
@@ -0,0 +1,7 @@
+package parksoffice.ojtcommunity.exception;
+
+public class PostNotFoundException extends RuntimeException {
+    public PostNotFoundException(String message) {
+        super(message);
+    }
+}
